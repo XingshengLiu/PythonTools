ddf5aa5f24b9aea9c2e806b2fc44b0081f3dd807
liuxingsheng <liuxingsheng@oaserver.dw.gdbbk.com>
2018-06-05 17:09:00 +0800

    1.规范函数名 2.修复不能生成文件的bug

--- reNameTool.py
+++ reNameTool.py
@@ -1,29 +1,29 @@
-import os, xlrd, xlsxwriter
+import os, xlrd, xlsxwriter, xlwt
 
 
 class FileContent:
 
-    def getattrfilname(self):
+    def getAttrFileName(self):
         return self.filename
 
-    def setattrfilename(self, filename):
+    def setAttrFileName(self, filename):
         self.filename = filename
 
-    def getattrcontent(self):
+    def getAttrContent(self):
         return self.content
 
-    def setattrcontent(self, content):
+    def setAttrContent(self, content):
         self.content = content
 
 
 class FileRename:
-    def getattrorigianlName(self):
+    def getAttrOrigianlName(self):
         return self.originalName
 
-    def setattroriginalName(self, originalName):
+    def setAttrOriginalName(self, originalName):
         self.originalName = originalName
 
-    def getChangeName(self):
+    def getChangedName(self):
         return self.changedName
 
     def setChangedName(self, mchangedName):
@@ -32,17 +32,22 @@ class FileRename:
 
 class FileTidyName:
 
-    def getattrnewlName(self):
+    def getAttrNewlName(self):
         return self.newName
 
-    def setattrnewName(self, newName):
+    def setAttrNewName(self, newName):
         self.newName = newName
 
-    def getcontent(self):
+    def getContent(self):
         return self.content
 
-    def setconten(self, content):
+    def setContent(self, content):
         self.content = content
+
+
+class MatchBean:
+    originalName = ''
+    tidyName = ''
 
 
 def getAllPCMFiles():
@@ -57,18 +62,37 @@ def getAllPCMFiles():
 
 
 def readFileNameAndRename():
-    batfile = open("rename.bat", "w")
-    pcmfileList = getAllPCMFiles()
-    i = 1
-    for pcm in pcmfileList:
-        if i < 10:
-            batfile.write(r'ren ' + pcm + '\t' + '00' + str(i) + '.pcm\n')
-        elif 10 <= i <= 99:
-            batfile.write(r'ren ' + pcm + '\t' + '0' + str(i) + '.pcm\n')
-        else:
-            batfile.write(r'ren ' + pcm + '\t' + str(i) + '.pcm\n')
-        i = i + 1
-    batfile.close()
+    matchBeanList = []
+    try:
+        batfile = open("rename.bat", "w")
+        pcmfileList = getAllPCMFiles()
+        i = 1
+        for pcm in pcmfileList:
+            matchBean = MatchBean()
+            matchBean.originalName = pcm
+            if i < 10:
+                batfile.write(r'ren ' + pcm + ' ' + '00' + str(i) + '.pcm\n')
+                matchBean.tidyName = '00' + str(i) + '.pcm'
+            elif 10 <= i <= 99:
+                batfile.write(r'ren ' + pcm + ' ' + '0' + str(i) + '.pcm\n')
+                matchBean.tidyName = '0' + str(i) + '.pcm'
+            else:
+                batfile.write(r'ren ' + pcm + ' ' + str(i) + '.pcm\n')
+                matchBean.tidyName = str(i) + '.pcm'
+            matchBeanList.append(matchBean)
+            i = i + 1
+        batfile.close()
+        row = 1
+        workbook = xlwt.Workbook(encoding='utf-8')
+        ws1 = workbook.add_sheet('Sheet1', cell_overwrite_ok=True)
+        for bean in matchBeanList:
+            ws1.write(row, 0, bean.originalName)
+            ws1.write(row, 1, bean.tidyName)
+            row = row + 1
+        workbook.save('changed.xls')
+    except IOError as ioerror:
+        print('文件读写错误:' + str(ioerror))
+    matchBeanList.clear()
 
 
 def readExcelFileContent():
@@ -80,8 +104,8 @@ def readExcelFileContent():
     rows = contenttest.nrows
     for row in range(1, rows):
         filecontent = FileContent()
-        filecontent.setattrcontent(contenttest.cell_value(row, 0))
-        filecontent.setattrfilename(contenttest.cell_value(row, 1))
+        filecontent.setAttrContent(contenttest.cell_value(row, 0))
+        filecontent.setAttrFileName(contenttest.cell_value(row, 1))
         fileContentList.append(filecontent)
     print("内容长度：", len(fileContentList))
     changedNamedata = xlrd.open_workbook(os.getcwd() + '\\changed.xls')
@@ -89,32 +113,36 @@ def readExcelFileContent():
     rows = changedtest.nrows
     for nrow in range(1, rows):
         filerename = FileRename()
-        filerename.setattroriginalName(changedtest.cell_value(nrow, 0))
+        filerename.setAttrOriginalName(changedtest.cell_value(nrow, 0))
         filerename.setChangedName(changedtest.cell_value(nrow, 1))
         fileRenameList.append(filerename)
-    print("name内容长度：",len(fileRenameList))
+    print("name内容长度：", len(fileRenameList))
     for contentitem in fileContentList:
         for nameitem in fileRenameList:
-            if contentitem.getattrfilname() == nameitem.getattrorigianlName():
+            if contentitem.getAttrFileName() == nameitem.getAttrOrigianlName():
                 filetidyname = FileTidyName()
-                filetidyname.setattrnewName(nameitem.getChangeName())
-                filetidyname.setconten(contentitem.getattrcontent())
+                filetidyname.setAttrNewName(nameitem.getChangedName())
+                filetidyname.setContent(contentitem.getAttrContent())
                 tidyList.append(filetidyname)
                 break
             else:
                 continue
-    print("tidyList的长度是：",len(tidyList))
+    print("tidyList的长度是：", len(tidyList))
     try:
         i = 1
-        workbook = xlsxwriter.Workbook(os.getcwd() + '\\' + 'result.xlsx')
-        ws = workbook.add_worksheet(u'sheet1')
+        workbook = xlwt.Workbook(encoding='utf-8')
+        ws = workbook.add_sheet('Sheet1', cell_overwrite_ok=True)
         ws.write(0, 0, 'content')
         ws.write(0, 1, 'filename')
+        ws.write(0, 2, 'id')
+        ws.write(0, 3, 'name')
         for resultitem in tidyList:
-            ws.write(i, 0, resultitem.getcontent())
-            ws.write(i, 1, resultitem.getattrnewlName())
+            ws.write(i, 0, resultitem.getContent())
+            ws.write(i, 1, resultitem.getAttrNewlName())
+            ws.write(i, 2, ' ')
+            ws.write(i, 3, ' ')
             i = i + 1
-        workbook.close()
+        workbook.save('result.xls')
     except IOError as ioerror:
         print("写入错误：" + str(ioerror))
     fileContentList.clear()
@@ -123,9 +151,8 @@ def readExcelFileContent():
 
 
 def main():
-    # readFileNameAndRename()
+    readFileNameAndRename()
     readExcelFileContent()
-
 
 
 if __name__ == '__main__':
